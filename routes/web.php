2fc12f3dfff919df46470670f363f81d1fddb271
Yazeed-Nazzal <yazeed_nazal@hotmail.com>
2021-12-14 23:00:19 +0200

    setup user layout

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\admin\dashboard;
+use App\Http\Controllers\user\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +17,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('dashboard',[Dashboard::class,'index']);
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[HomeController::class,'index']);
