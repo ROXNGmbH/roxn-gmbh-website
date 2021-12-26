88d1caa350e0233a101d0254d99f24682cc8b030
Yazeed-Nazzal <yazeed_nazal@hotmail.com>
2021-12-26 23:14:13 +0200

    product migration

--- database/migrations/2021_12_25_204730_create_products_table.php
+++ database/migrations/2021_12_25_204730_create_products_table.php
@@ -15,6 +15,29 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->json('name');
+            $table->json('description');
+            $table->string('slug');
+            $table->boolean('status');
+            $table->double('price');
+            $table->double('purchase_price');
+            $table->double('offer_price')->nullable();
+            $table->integer('gty');
+            $table->integer('min_gty')->nullable();
+            $table->integer('max_gty')->nullable();
+            $table->integer('weight');
+            $table->json('tages')->nullable();
+            $table->string('barcode',50)->nullable();
+            $table->foreignId('delivery_time_id')->constrained();
+            $table->foreignId('tax_id')->constrained();
+            $table->foreignId('unit_id')->constrained();
+            $table->foreignId('category_id')->constrained();
+            $table->foreignId('sub_category_id')->constrained();
+            $table->foreignId('rate_id')->constrained();
+            $table->foreignId('manfacturing_company_id')->constrained();
+            $table->foreignId('sell_type_id')->constrained();
+            $table->foreignId('flag_id')->constrained();
+            $table->timestamps();
         });
     }
 
