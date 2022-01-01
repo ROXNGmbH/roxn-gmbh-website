2cee91b22216d6182081e416d13cfa8af0ecccc2
Yazeed-Nazzal <yazeed_nazal@hotmail.com>
2022-01-01 16:47:00 +0200

    delivery-times table edit

--- resources/views/pages/admin/delivery-time/index.blade.php
+++ resources/views/pages/admin/delivery-time/index.blade.php
@@ -14,45 +14,50 @@
 @stop
 
 @section('main-content')
-    <div class="card mb-4">
-        <div class="card-body">
-            <article class="itemlist">
-                <div class="row align-items-center">
-                    <div class="col-lg-3 col-sm-2 col-4">
-                        #ID
-                    </div>
-                    <div class="col-lg-3 col-sm-2 col-4">
-                        Time
-                    </div>
-                    <div class="col-lg-3 col-sm-2 col-4 col-action text-center">
-                        Action
-                    </div>
-                </div>
-            </article>
+    <div class="p-4 bg-white">
+        <table class="table">
+            <thead>
+            <tr>
+                <th scope="col">#ID</th>
+                <th scope="col">Time</th>
+                <th scope="col" class="text-center">Action</th>
+            </tr>
+            </thead>
+            <tbody>
             @foreach($times as $time)
-                <article class="itemlist">
-                    <div class="row align-items-center">
-                        <div class="col-lg-3 col-sm-2 col-4 ">
-                            {{$time->id}}
+                <tr>
+                    <td>{{$time->id}}</td>
+                    <td>{{$time->from}} - {{$time->to}}  {{$time->type}}</td>
+                    <td class="d-flex justify-content-center">
+                        <a href="{{route('delivery-times.edit',$time->id)}}" class="btn btn-sm font-sm rounded btn-brand mx-2"> <i class="material-icons md-edit"></i> Edit </a>
+                        <button type="button" class="btn btn-sm font-sm btn-light rounded mx-2" data-bs-toggle="modal" data-bs-target="#delete-{{$time->id}}">
+                            <i class="material-icons md-delete_forever"></i> Delete
+                        </button>
+
+                        <div class="modal fade" id="delete-{{$time->id}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">Delete Category</h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <p>Are you sure to delete delivery time ?</p>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-sm font-sm rounded btn-light mx-2" data-bs-dismiss="modal">Close</button>
+                                        <form action="{{route('delivery-times.destroy',$time->id)}}" method="post">
+                                            @csrf
+                                            @method('delete')
+                                            <button type="submit" class="btn btn-sm font-sm rounded btn-brand mr-2">Delete</button>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <div class="col-lg-3 col-sm-2 col-4">
-                            {{$time->from}} - {{$time->to}}  {{$time->type}}
-                        </div>
-                        <div class="col-lg-3 col-sm-2 col-4 col-action text-center d-flex justify-content-center ">
-                            <a href="{{route('delivery-times.edit',$time->id)}}" class="btn btn-sm font-sm rounded btn-brand mx-2"> <i class="material-icons md-edit"></i> Edit </a>
-                            <form action="{{route('delivery-times.destroy',$time->id)}}" method="post">
-                                @csrf
-                                @method('delete')
-                                <button class="btn btn-sm font-sm btn-light rounded">
-                                    <i class="material-icons md-delete_forever"></i> Delete
-                                </button>
-                            </form>
-                        </div>
-                    </div>
-                    <!-- row .// -->
-                </article>
+                    </td>
+                </tr>
             @endforeach
-        </div>
-        <!-- card-body end// -->
+            <!-- card-body end// -->
     </div>
 @stop
