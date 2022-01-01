5dea177f036751965acf2318205efe4b5d05fa32
Abdallah Edrees Abu Karaki <53639288+Abdallah-abu-karaki@users.noreply.github.com>
2022-01-01 14:56:06 +0200

    testing 2 step 1

--- resources/views/pages/admin/sell-type/create.blade.php
+++ resources/views/pages/admin/sell-type/create.blade.php
@@ -13,24 +13,26 @@
 @section('main-content')
     <form action="{{route('sell-types.store')}}" method="post">
         @csrf
-        <div class="row justify-content-start">
-            <div class="col-md-6">
-                <div class="card mb-4">
-                    <div class="card-header">
-                        <h4>Information</h4>
+        <div class="card mb-4">
+            <div class="card-header">
+                <h4>Information</h4>
+            </div>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <label for="name_de" class="mb-2">Name (de)</label>
+                        <input type="text" name="name_de" id="name_de" class="form-control" placeholder="Enter sell type name de"><br>
                     </div>
-                    <div class="card-body">
-                           <div>
-                               <label for="name">Name DE</label>
-                               <input type="text" name="name_de" id="name" class="form-control"><br>
-
-                           </div>
-                           <div>
-                             <label for="name">Name AR</label>
-                             <input type="text" name="name_ar" id="name" class="form-control"><br>
-                           </div>
+                    <div class="col-lg-6">
+                        <label for="name_ar" class="mb-2">Name (ar)</label>
+                        <input type="text" name="name_ar" id="name_ar" class="form-control" placeholder="Enter sell type name ar"><br>
+                    </div>
+                </div>
+                <div class="row align-items-end justify-content-end">
+                    <div class="col-lg-12">
+                        <div class="d-flex justify-content-end pt-2 mt-5">
                             <button class="btn btn-primary" type="submit">Save</button>
-
+                        </div>
                     </div>
                 </div>
             </div>
